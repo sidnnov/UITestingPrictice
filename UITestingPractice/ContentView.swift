64103a9708d0ebac35f70d1c651a8511256b926b
Tylechkin <qa_works@mail.ru>
2025-11-04 12:30:46 +0300

    Initial Commit

---
+++ UITestingPractice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UITestingPractice
+//
+//  Created by Tylechkin.Yuriy on 04.11.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UITestingPractice/UITestingPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  UITestingPracticeApp.swift
+//  UITestingPractice
+//
+//  Created by Tylechkin.Yuriy on 04.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct UITestingPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
