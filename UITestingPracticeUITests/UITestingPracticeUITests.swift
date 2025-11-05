d938c7d517471acd4ae53eef5e55af34b3887024
Tylechkin <qa_works@mail.ru>
2025-11-05 09:59:41 +0300

    add UI test

--- UITestingPracticeUITests/UITestingPracticeUITests.swift
+++ UITestingPracticeUITests/UITestingPracticeUITests.swift
@@ -26,8 +26,35 @@ final class UITestingPracticeUITests: XCTestCase {
     func testExample() throws {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
-        app.launch()
 
+        let emailField = app.textFields["emailField"]
+        let passwordField = app.secureTextFields["passwordField"]
+        let loginButton = app.buttons["loginButton"]
+        
+        app.launch()
+        
+        //Проверка элементов на экране
+        XCTAssertTrue(emailField.exists, "Поле email должно быть на экране")
+        XCTAssertTrue(passwordField.exists, "Поле пароля должно быть на экране")
+        XCTAssertTrue(loginButton.exists, "Поле входа должно быть на экране")
+        
+        emailField.tap()
+        emailField.typeText("test@test.com")
+        
+        passwordField.tap()
+        passwordField.typeText("123456")
+        
+        loginButton.tap()
+        
+        let welcomeText = app.staticTexts["welcomeText"]
+        
+        let exists = welcomeText.waitForExistence(timeout: 5)
+        XCTAssertTrue(exists, "После входа должен появится приветственный текст")
+        
+        if exists {
+            XCTAssertEqual(welcomeText.label, "Добро пожаловать!")
+        }
+        
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
