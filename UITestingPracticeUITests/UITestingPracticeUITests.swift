bf2559a569e42a47ff5a6dcebcec8f6e09f8d638
Tylechkin <qa_works@mail.ru>
2025-11-05 21:18:05 +0300

    Add UI test empty fields

--- UITestingPracticeUITests/UITestingPracticeUITests.swift
+++ UITestingPracticeUITests/UITestingPracticeUITests.swift
@@ -23,8 +23,7 @@ final class UITestingPracticeUITests: XCTestCase {
     }
 
     @MainActor
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    func testAuthorization() throws {
         let app = XCUIApplication()
 
         let emailField = app.textFields["emailField"]
@@ -36,14 +35,17 @@ final class UITestingPracticeUITests: XCTestCase {
         //Проверка элементов на экране
         XCTAssertTrue(emailField.exists, "Поле email должно быть на экране")
         XCTAssertTrue(passwordField.exists, "Поле пароля должно быть на экране")
-        XCTAssertTrue(loginButton.exists, "Поле входа должно быть на экране")
+        XCTAssertTrue(loginButton.exists, "Кнопка входа должна быть на экране")
         
+        //Ввод в поле email
         emailField.tap()
         emailField.typeText("test@test.com")
         
+        //Ввод в поле пароль
         passwordField.tap()
         passwordField.typeText("123456")
         
+        //Нажатие кнопки войти
         loginButton.tap()
         
         let welcomeText = app.staticTexts["welcomeText"]
@@ -55,7 +57,42 @@ final class UITestingPracticeUITests: XCTestCase {
             XCTAssertEqual(welcomeText.label, "Добро пожаловать!")
         }
         
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    @MainActor
+    func testAuthorizationFailed() throws {
+        let app = XCUIApplication()
+
+        let emailField = app.textFields["emailField"]
+        let passwordField = app.secureTextFields["passwordField"]
+        let loginButton = app.buttons["loginButton"]
+        
+        app.launch()
+        
+        //Проверка элементов на экране
+        XCTAssertTrue(emailField.exists, "Поле email должно быть на экране")
+        XCTAssertTrue(passwordField.exists, "Поле пароля должно быть на экране")
+        XCTAssertTrue(loginButton.exists, "Кнопка входа должна быть на экране")
+        
+        //Ввод в поле email
+        emailField.tap()
+        emailField.typeText("test@test.com")
+        
+        //Нажатие кнопки войти
+        loginButton.tap()
+        
+        let generalErrorText = app.staticTexts["generalErrorText"]
+        XCTAssertEqual(generalErrorText.label, "Пожалуйста, введите пароль.")
+        
+        //Ввод в поле пароль
+        passwordField.tap()
+        passwordField.typeText("12345")
+        
+        //Нажатие кнопки войти
+        loginButton.tap()
+        //Проверка на невалидный пароль
+        XCTAssertEqual(generalErrorText.label, "Неверный email или пароль.")
+        
     }
 
     @MainActor
